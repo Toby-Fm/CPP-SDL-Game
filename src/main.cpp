6694c57a6815f067234ba9296ab5711f696ca0db
Toby-Fm <mail@toby-wichmann.de>
2023-09-27 23:29:24 +0200

    Create main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <headers/game.h> // Inkludiert die Header-Datei "game.h", die die Deklaration der Game-Klasse enthält.
+
+int main(int argc, char* argv[]) {
+    Game game; // Erzeugt eine Instanz der Game-Klasse, um das Spiel zu initialisieren und auszuführen.
+
+    game.run(); // Startet die Hauptfunktion des Spiels, die die Spiel-Initialisierung und die Hauptschleife enthält.
+
+    return 0; // Gibt eine Erfolgsmeldung (0) zurück, um anzuzeigen, dass das Spiel ohne Fehler beendet wurde.
+}
